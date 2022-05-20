e14939db40b9e16aee9675abddd58b1bc131627b
mr-meetpatel <pm2410.ldrp@gmail.com>
2022-05-20 19:01:18 +0530

    final commit

---
+++ src/Udemy/Exercise_29.kt
@@ -0,0 +1,16 @@
+/**
+ * Create a function that takes a product name and price. It adds 20% tax to that product and displays a message saying how much the product costs.
+ * Overload the function so that it takes a map of products and prices, and does the same thing.
+ * Call both functions in your program.
+ */
+fun main(args: Array<String>) {
+    helloClient(5,"Meet","Amesha","Jay")
+}
+fun helloClient(count:Int,vararg name:String){
+    for(name in name){
+        for(i in 1..count)
+            print("Hello $name :")
+        println()
+    }
+}
+
